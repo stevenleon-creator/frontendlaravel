f9ed7f324b39bfb9a7da266275f983683e931233
steven-leon <stevl4730@gmail.com>
2026-09-24 21:33:21 -0500

    fix: usar variable de entorno VITE_API_URL en vez de URL hardcodeada

--- src/services/aprendices.service.js
+++ src/services/aprendices.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = "http://127.0.0.1:8000/api/aprendices";
+const API_BASE = `${import.meta.env.VITE_API_URL}/aprendices`;
 
 export const getAprendices = async () => {
   const res = await axios.get(API_BASE);
